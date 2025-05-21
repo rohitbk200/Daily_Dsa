9654b2f2f54ba5009d85efb27e00762efad2380f
Rohit <rohitbiswakarma117@gmail.com>
2025-05-20 23:53:46 -0500

    lc updated problem 283 solution

--- lc283.cpp
+++ lc283.cpp
@@ -1,29 +1,30 @@
-##ZEROS AT END
-##2 POINTER APPROACH
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-     int i,j=-1,temp;
-     for(int i=0;i<nums.size();i++){
-        if(nums[i]==0){
-          j=i;
-          break;
-        } 
-        if(j==-1) return ; 
+        int j = -1, temp;
 
-     ##FIND THE INDEX OF FIRST ZERO
-     }
-     for(int i=j+1;i<nums.size();i++){
-        if(nums[i]!=0){
-            temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            j++;   
+        // First loop: Find the index of the first zero in the array
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i; // Store the position of the first zero
+                break; // Exit loop once first zero is found
+            }
         }
-     }
-     return ;
+
+        // If there are no zeros, array is already in desired state
+        if (j == -1) return;
+
+        // Second loop: Traverse the rest of the array from j+1
+        for (int i = j + 1; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                // Swap current non-zero element with the element at position j
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                // Move j to the next position where a zero exists
+                j++;
+            }
+        }
     }
-
-    ##SWAP WHENEVER I IS NOT ZERO
 };
-
